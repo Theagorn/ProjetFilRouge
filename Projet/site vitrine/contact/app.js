4b376e5c914979e7ab4910aae53350a9cd4a737a
Theagorn <119487168+Theagorn@users.noreply.github.com>
2023-09-22 16:04:27 +0200

    update

--- Projet/site vitrine/contact/app.js
+++ Projet/site vitrine/contact/app.js
@@ -13,3 +13,20 @@ document.getElementById("retourHaut").addEventListener("click", function () {
         behavior: "smooth"
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        console.log(`Nom: ${name}`);
+        console.log(`Email: ${email}`);
+        console.log(`Message: ${message}`);
+    });
+});
