c5c9c2cf2306dc78aaf890568434e545c165fa0f
Theagorn <119487168+Theagorn@users.noreply.github.com>
2024-01-29 16:16:05 +0100

    update

--- quiz/BDD/app.js
+++ quiz/BDD/app.js
@@ -1,7 +1,7 @@
 //pour démarrer le serveur MySQL, chercher MySQL dans Services windows
 //pour le modifier, utiliser un terminal et 'mysql -u root -p'
 
-//pour node, faire cd D:\Codes\ProjetFilRouge\Projet\quiz\BDD
+//pour node, faire cd D:\Codes\ProjetFilRouge\quiz\BDD
 //node app.js
 
 //sur mysql workbench : 
@@ -33,20 +33,26 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/choix.html');
 });
 
+// Affichage du formulaire de connexion lorsque l'utilisateur accède à /connexion en utilisant la méthode GET
 app.get('/connexion', (req, res) => {
     res.sendFile('login.html');
 });
 
-//Formulaire de connexion
+// Traitement du formulaire de connexion soumis par l'utilisateur en utilisant la méthode POST
 app.post('/connexion', (req, res) => {
+    // Extraction des données du formulaire depuis le corps de la requête
     const { email, mot_de_passe } = req.body;
 
+    // Obtention d'une connexion à la base de données depuis le pool
     pool.getConnection((err, connection) => {
         if (err) throw err;
 
-        connection.query('SELECT * FROM utilisateur WHERE email_utilisateur = ? AND mdp_utilisateur = ?', [email, mot_de_passe], (err, results) => {
+        // Exécution d'une requête SQL pour vérifier les informations de connexion dans la base de données
+        connection.query('SELECT * FROM utilisateur WHERE email_utilisateur = ? AND mdp_utilisateur = ?',
+        [email, mot_de_passe], (err, results) => {
             if (err) throw err;
 
+            // Vérification des résultats de la requête
             if (results.length > 0) {
                 console.log('Connexion réussie');
                 res.send('Connexion réussie');
@@ -54,33 +60,45 @@ app.post('/connexion', (req, res) => {
                 console.log('Échec de la connexion');
                 res.send('Échec de la connexion');
             }
+
+            // Libération de la connexion pour la rendre à nouveau disponible dans le pool
             connection.release();
         });
     });
-
 });
 
+
+// Affichage du formulaire d'inscription lorsque l'utilisateur accède à /inscription en utilisant la méthode GET
 app.get('/inscription', (req, res) => {
     res.sendFile('inscription.html');
 });
 
-// Formulaire d'inscription
+// Traitement du formulaire d'inscription soumis par l'utilisateur en utilisant la méthode POST
 app.post('/inscription', (req, res) => {
+    // Extraction des données du formulaire depuis le corps de la requête
     const { nom, email, mot_de_passe } = req.body;
 
+    // Obtention d'une connexion à la base de données depuis le pool
     pool.getConnection((err, connection) => {
+        if (err) throw err;
 
-        connection.query('INSERT INTO utilisateur (pseudo_utilisateur, email_utilisateur, mdp_utilisateur) VALUES (?, ?, ?)', [nom, email, mot_de_passe], (err, results) => {
+        // Exécution d'une requête SQL pour insérer les informations d'inscription dans la base de données
+        connection.query('INSERT INTO utilisateur (pseudo_utilisateur, email_utilisateur, mdp_utilisateur) VALUES (?, ?, ?)', 
+        [nom, email, mot_de_passe], (err, results) => {
             if (err) throw err;
 
+            // Si l'insertion réussit, afficher un message de succès
             console.log('Inscription réussie');
             res.send('Inscription réussie');
-        });
 
-        connection.release();
+            // Libération de la connexion pour la rendre à nouveau disponible dans le pool
+            connection.release();
+        });
     });
 });
+
 
 app.listen(port, () => {
     console.log(`Serveur démarré sur le port ${port}`);
 });
+
